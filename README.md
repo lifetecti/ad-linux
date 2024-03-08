897bf76a26d83b36d7a7d7fb4a83898fc420c105
lifetecti <139556710+lifetecti@users.noreply.github.com>
2024-03-08 15:23:58 -0300

    Adicionado scripts e README

---
+++ README.md
@@ -0,0 +1,37 @@
+# Shell script para ingressar uma máquina linux ao AD
+
+## Ubuntu
+
+Entre no servidor como root ou eleve seus privilégios com sudo.
+
+Baixe o arquivo sh referente ao ubuntu
+
+```shell
+wget https://raw.githubusercontent.com/lifetecti/ad-linux/main/ubuntu.sh
+```
+Em seguida faça o arquivo ser executável
+```shell
+chmod +x ubuntu.sh
+```
+Agora só executar o arquivo e fornecer as informações conforme forem solicitadas.
+```shell
+./ubuntu.sh
+```
+
+## CentOS
+
+Entre no servidor como root ou eleve seus privilégios com sudo.
+
+Baixe o arquivo sh referente ao centOS.
+
+```shell
+wget https://raw.githubusercontent.com/lifetecti/ad-linux/main/centos.sh
+```
+Em seguida faça o arquivo ser executável.
+```shell
+chmod +x centos.sh
+```
+Agora só executar o arquivo e fornecer as informações conforme forem solicitadas.
+```shell
+./centos.sh
+```
---
+++ permissions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Testa se o usuário que está tentando rodar o script tem permissão de root
+if [ "$EUID" -ne 0 ]
+	then echo "Por favor rodar como root"
+	exit
+fi
+#read -p "Digite o domínio que a máquina está inserida: " domain
+
+echo "Fazendo backup do arquivo de sudoers..."
+
+cp /etc/sudoers /etc/sudoers.bkp
+
+echo "Editando o arquivo de sudoers..."
+
+cp /etc/sudoers /tmp/sudoers.tmp
+
+cat << EOF >> /tmp/sudoers.tmp
+
+#Alias de comandos
+Cmnd_Alias NOROOT = !/usr/bin/su, !/bin/bash
+Cmnd_Alias VIEW_IPTABLES= /usr/sbin/iptables -L, /usr/sbin/iptables -nL
+Cmnd_Alias ASTERISK = /usr/sbin/asterisk, /usr/sbin/rasterisk
+
+#Grupos definidos no AD
+#%life_administradores@${domain}    ALL=(ALL) ALL, NOROOT
+#%life_operador@${domain}	    ALL=(ALL) ALL, NOROOT, !/sbin/shutdown, !/sbin/reboot, !/usr/bin/rm
+#%life_viewer@${domain}		    ALL=(ALL) /usr/bin/cat, VIEW_IPTABLES, ASTERISK
+
+%life_administradores	    ALL=(ALL) ALL, NOROOT
+%life_operador  	    ALL=(ALL) ALL, NOROOT, !/sbin/shutdown, !/sbin/reboot, !/usr/bin/rm
+%life_viewer		    ALL=(ALL) /usr/bin/cat, VIEW_IPTABLES, ASTERISK
+
+EOF
+
+mv -f /tmp/sudoers.tmp /etc/sudoers
+
+echo "Finalizado configuração"
+
+
+
